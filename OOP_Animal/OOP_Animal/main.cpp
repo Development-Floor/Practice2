1d03288f96c12bea49a765f75cb796c9a7240a07
Minwoo <ymw1023@kakao.com>
2024-12-26 14:26:39 +0900

    Create the class

--- OOP_Animal/OOP_Animal/main.cpp
+++ OOP_Animal/OOP_Animal/main.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
+class Animal {
+
+
+};
+
+class Dog : public Animal {
+
+
+};
+
+class Cat : public Animal {
+
+
+};
+
+class Cow : public Animal {
+
+};
+
 int main() {
 
 
